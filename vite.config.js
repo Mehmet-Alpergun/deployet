75af8b2af5763b0ab7aad4dda4d606afa45751fe
Mehmet Alpergun <mehmetalpergunn@gmail.com>
2025-10-25 09:39:09 +0300

    asdasdasd

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        index: resolve(__dirname, "index.html"),
         menu: resolve(__dirname, "menu.html"),
         cart: resolve(__dirname, "cart.html"),
         register: resolve(__dirname, "register.html"),
